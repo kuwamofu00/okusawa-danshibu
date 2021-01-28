5103870c789d34b110f272f99e7a9c36906865d6
kuwamofu <88776655443322wot@gmail.com>
2021-01-28 12:55:03 +0900

    add message

--- index.php
+++ index.php
@@ -30,9 +30,9 @@
   <div class="navbar navbar-dark bg-dark shadow-sm">
     <div class="container d-flex justify-content-between">
       <a href="#" class="navbar-brand d-flex align-items-center">
-       <img src="/rogo-top.PNG">   
+       <img src="sanshokuki.png">   
 <!--        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="mr-2" viewBox="0 0 24 24" focusable="false"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>-->
-        <strong class="ml-3">ヘッダーのメッセージsample </strong>
+        <strong class="ml-3">setagaya okusawa </strong>
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -45,11 +45,18 @@
 
   <section class="jumbotron text-center">
     <div class="container">
-      <h1>創価学会奥沢男子部</h1>
-      <p class="lead text-muted">ここにメッセージを sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample </p>
+      <h1>奥沢男子部の研鑽物語</h1>
+      <p class="lead text-muted text-left" >
+        </br>概要：毎週日曜20：20分より開催される 小説「新・人間革命」を研鑽する活動。
+        </br>「新・人間革命」とは戸田城聖の後を継ぎ、創価学会第三代会長となった山本伸一の峻厳(しゅんげん)な「弟子の道」が綴られている。
+        日蓮大聖人の仏法のヒューマニズムの光をかかげて、世界を舞台に繰り広げられる民衆凱歌の大河小説。
+        <p class="lead text-muted text-center" >部長メッセージ：「共に研鑽し、実生活の中で生かして参りましょう!!!」</p>
+        </br><p class="lead-text-muted-o">〈注意事項〉以下の行為はご遠慮願います。
+        </br>不特定多数の方が閲覧できるブログやSNSへの投稿、録画し内外に展開すること。</p>
+      </p>
       <p>
-        <a href="https://mega.nz/file/W0MFAAjY#bIq5s6DhJUkIoKAHbHZy9OCDKMFRs08WNYxYuYtLHck" class="btn btn-primary my-2">例：action or jump</a>
-        <a href="#" class="btn btn-secondary my-2">例：action or jump</a>
+        <a href="https://mega.nz/file/W0MFAAjY#bIq5s6DhJUkIoKAHbHZy9OCDKMFRs08WNYxYuYtLHck" class="btn btn-primary my-2">工事中</a>
+        <a href="#" class="btn btn-secondary my-2">工事中</a>
       </p>
     </div>
   </section>
@@ -85,7 +92,15 @@
               <span class="d-block text-center" style="width: 100%;"><a href="https://mega.nz/file/PktUGSKB#b_sqJt9Vt3AjLNbp8pXapghEV4W0B8Jc2MIH5P8TpgE" target="_blank">2020/12/27(日)</br>奥沢部第２回研鑽会</br>新・人間革命第１巻『旭日』の章①</a></span>  
             </div>
             <div class="card-body">
-              <p class="card-text">動画の紹介メッセージを入力　sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample</p>
+              <p class="card-text">『新・人間革命』第1巻が発刊されたのは1998年（平成10年）の1月2日。池田先生が70歳の古希を迎えた日です。
+この2日後の1月4日付の聖教新聞に、第1回となる「随筆　新・人間革命」が掲載されました。
+その中で先生はご自身の来し方を10年ごとに振り返りつつ、未来を展望されます。
+前作の小説『人間革命』は、「黎明」の章から始まります。これに対して『新・人間革命』は、「旭日」の章からスタートします。
+「黎明」「旭日」は、どちらも太陽が関係する表現ではありますが、意味合いが少し異なります。
+「黎明」とは、夜明け。この黎明の後に昇るのが「旭日」です。
+『人間革命』では、戸田先生の心の中だけに「黎明」があったと述べられています。つまり、
+戸田先生お一人の心から始まった「地涌の菩薩」としての自覚と実践が、弟子・山本伸一に受け継がれ、
+旭日の勢いで、世界へ広宣流布が広がっていったのです。</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                   <a class="btn btn-sm btn-outline-secondary" href="https://mega.nz/file/PktUGSKB#b_sqJt9Vt3AjLNbp8pXapghEV4W0B8Jc2MIH5P8TpgE" target="_blank">見る</a>
